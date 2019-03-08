e244661500d00c38ba2144900cbf80578c3f0767
Piotr Nowinski <pitred007git@gmail.com>
2019-03-08 19:05:57 +0100

    FIX some problems with logic in script.js

--- js/script.js
+++ js/script.js
@@ -5,26 +5,24 @@ var scissorsBtn = document.getElementById('scissors');
 var output = document.getElementById('output');
 
 //function that selects random number from 1 to 3
-function randomChoice() {
-    var computerChoice = Math.ceil(Math.random() * 3);
-    return computerChoice;
+function computerMove() {
+    var randomNumber = Math.ceil(Math.random() * 3);
+    if (randomNumber === 1) return 'ROCK';
+    else if (randomNumber === 2) return 'PAPER';
+    else return 'SCISSORS';
 }
 
 //function that shows player moves
 function playerMove(playerChoice) {
-    computerChoice = randomChoice();
-    if (computerChoice == 1) return computerChoice = 'ROCK';
-    else if (computerChoice == 2) return computerChoice = 'PAPER';
-    else return computerChoice = 'SCISSORS';
-    playerChoice = playerMove();
+    var computerChoice = computerMove();
+    outputResult(computerChoice, playerChoice);
 }
 
-// -- TO FIX (CONDITION) --
 //function that shows output results
 function outputResult(computerChoice, playerChoice) {
-    if (computerChoice == playerChoice) {
+    if (computerChoice === playerChoice) {
         output.innerHTML = '<br>THERE IS A TIE: you played a ' + playerChoice + ', computer played ' + computerChoice;
-    } else if (computerChoice == 2) {
+    } else if ((playerChoice === 'ROCK' && computerChoice === 'SCISSORS') || (playerChoice === 'PAPER' && computerChoice === 'ROCK') || (playerChoice === 'SCISSORS' && computerChoice === 'PAPER')) {
         output.innerHTML = '<br>YOU WON: you played a ' + playerChoice + ', computer played ' + computerChoice;
     } else {
         output.innerHTML = '<br>YOU LOST: you played a ' + playerChoice + ', computer played ' + computerChoice;
@@ -33,18 +31,15 @@ function outputResult(computerChoice, playerChoice) {
 
 // reaction at the press of a button rock
 rockBtn.addEventListener('click', function () {
-    playerMove();
-    outputResult(computerChoice, 'ROCK');
+    playerMove('ROCK');
 });
 
 // reaction at the press of a button paper
 paperBtn.addEventListener('click', function () {
-    playerMove();
-    outputResult(computerChoice, 'PAPER');
+    playerMove('PAPER');
 });
 
 // reaction at the pres s of a button scissors
 scissorsBtn.addEventListener('click', function () {
-    playerMove();
-    outputResult(computerChoice, 'SCISSORS');
+    playerMove('SCISSORS');
 });
