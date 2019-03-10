3d3a98f748b395efaf5fd7183b7817e85f2a9283
Piotr Nowinski <pitred007git@gmail.com>
2019-03-10 21:43:44 +0100

    Add some function and style to project

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
 //Variables
-var rockBtn = document.getElementById('rock');
-var paperBtn = document.getElementById('paper');
-var scissorsBtn = document.getElementById('scissors');
-var newGameBtn = document.getElementById('new-game');
-var outputWinStatus = document.getElementById('output');
-var outputPoints = document.getElementById('result');
-var outputNrRounds = document.getElementById('rounds-to-win');
+var rockBtn = document.getElementById("rock");
+var paperBtn = document.getElementById("paper");
+var scissorsBtn = document.getElementById("scissors");
+var newGameBtn = document.getElementById("new-game");
+var outputWinStatus = document.getElementById("output");
+var outputPoints = document.getElementById("result");
+var outputNrRounds = document.getElementById("rounds-to-win");
 var won = 0;
 var lost = 0;
-var result = 0;
 var conditionToWin;
+
 
 // function that ask for number of rounds
 function askForRounds() {
-    var conditionToWin = prompt('How many wins the rounds ends the game ?');
-    if ((isNaN(conditionToWin) || conditionToWin <= 0)) {
-        alert('Enter NUMBER of rounds !');
+    var condition = prompt("How many winning rounds end the game ?");
+    conditionToWin = Number(condition);
+    if (isNaN(conditionToWin) || conditionToWin <= 0) {
+        alert("Enter NUMBER of rounds !");
     } else {
-        outputNrRounds.innerHTML = 'ROUNDS TO WIN : ' + conditionToWin;
+        outputNrRounds.innerHTML = "ROUNDS TO WIN : " + conditionToWin;
         return conditionToWin;
     }
 }
 
-function endOfTheGame() {
-    var winOrLost = conditionToWin;
-    if (winOrLost === won) {
-        outputNrRounds.innerHTML = 'YOU WIN ENTIRE GAME';
-    } else if (winOrLost === lost) {
-        outputNrRounds.innerHTML = 'COMPUTER WIN ENTIRE GAME';
+// function that ends game
+function endOfTheGame(conditionToWin, won, lost) {
+    if (conditionToWin === won) {
+        outputNrRounds.innerHTML = "<strong>YOU WIN ENTIRE GAME</strong>";
+    } else if (conditionToWin === lost) {
+        outputNrRounds.innerHTML = "<strong>COMPUTER WIN ENTIRE GAME</strong>";
     }
 }
-console.log(endOfTheGame());
 
 // function that selects random number from 1 to 3
 function computerMove() {
     var randomNumber = Math.ceil(Math.random() * 3);
-    if (randomNumber === 1) return 'ROCK';
-    else if (randomNumber === 2) return 'PAPER';
-    else return 'SCISSORS';
+    if (randomNumber === 1) return "ROCK";
+    else if (randomNumber === 2) return "PAPER";
+    else return "SCISSORS";
 }
 
 // function that shows player moves
@@ -49,34 +49,34 @@ function playerMove(playerChoice) {
 // function that shows output results
 function outputResult(computerChoice, playerChoice) {
     if (computerChoice === playerChoice) {
-        outputWinStatus.innerHTML = '<br>THERE IS A TIE: you played a ' + playerChoice + ', computer played ' + computerChoice;
-    } else if ((playerChoice === 'ROCK' && computerChoice === 'SCISSORS') || (playerChoice === 'PAPER' && computerChoice === 'ROCK') || (playerChoice === 'SCISSORS' && computerChoice === 'PAPER')) {
-        outputWinStatus.innerHTML = '<br>YOU WON: you played a ' + playerChoice + ', computer played ' + computerChoice;
+        outputWinStatus.innerHTML = "<br>THERE IS A TIE: you played a " + playerChoice + ", computer played " + computerChoice;
+    } else if ((playerChoice === "ROCK" && computerChoice === "SCISSORS") || (playerChoice === "PAPER" && computerChoice === "ROCK") || (playerChoice === "SCISSORS" && computerChoice === "PAPER")) {
+        outputWinStatus.innerHTML = "<br>YOU WON: you played a " + playerChoice + ", computer played " + computerChoice;
         won += 1;
     } else {
-        outputWinStatus.innerHTML = '<br>YOU LOST: you played a ' + playerChoice + ', computer played ' + computerChoice;
+        outputWinStatus.innerHTML = "<br>YOU LOST: you played a " + playerChoice + ", computer played " + computerChoice;
         lost += 1;
     }
-    outputPoints.innerHTML = 'PLAYER &nbsp' + won + ' - ' + lost + '&nbsp COMPUTER';
+    outputPoints.innerHTML = "PLAYER &nbsp" + won + " - " + lost + "&nbsp COMPUTER";
+    endOfTheGame(conditionToWin, won, lost);
 }
 
 // react at the press of a button new game
-newGameBtn.addEventListener('click', function () {
+newGameBtn.addEventListener("click", function () {
     askForRounds();
 });
 
 // reaction at the press of a button rock
-rockBtn.addEventListener('click', function () {
-    playerMove('ROCK');
+rockBtn.addEventListener("click", function () {
+    playerMove("ROCK");
 });
 
 // reaction at the press of a button paper
-paperBtn.addEventListener('click', function () {
-    playerMove('PAPER');
+paperBtn.addEventListener("click", function () {
+    playerMove("PAPER");
 });
 
 // reaction at the pres s of a button scissors
-scissorsBtn.addEventListener('click', function () {
-    playerMove('SCISSORS');
-    endOfTheGame();
+scissorsBtn.addEventListener("click", function () {
+    playerMove("SCISSORS");
 });
